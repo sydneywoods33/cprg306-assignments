04e2bbf67040c6367a64ccd0a9b399c97cb0dbe1
sydneywoods33 <sydneywoods50@gmail.com>
2024-09-13 09:37:29 -0600

    create week-2
    cleaned up home page, created week 2 folder / page

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+export default function ShoppingList() {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <ul>
+        <li>Apples</li>
+        <li>Oranges</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+}
